e10d32f211afde866fccecbbdaeb21d8e252c03e
amanullayev2210 <karory577@gmail.com>
2025-02-11 00:47:03 +0500

    Update JS

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,29 @@
 const form = document.querySelector(".js-form")
 const input = document.querySelector(".js-input");
 const wrapper = document.querySelector(".js-wrapper");
+let num = 1;
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const p = document.createElement("p");
-  p.textContent = input.value;
+  p.textContent = `${num} ${input.value}`;
   wrapper.appendChild(p);
-
   input.value = null;
+  num++;
 });
+
+// const form = document.querySelector(".js-form")
+// const  input = document.querySelector(".js-input");
+// const wrapper = document.querySelector(".js-ul");
+// const template = document.querySelector(".js-template").content;
+
+// form.addEventListener("submit", (evt) => {
+//   evt.preventDefault();
+//   const docFrg = document.createDocumentFragment();
+//   const clone = template.cloneNode(true);
+//   clone.querySelector(".js-li").textContent = input.value;
+//   docFrg.appendChild(clone);
+//   wrapper.appendChild(docFrg);
+
+//   input.value = null;
+// });
