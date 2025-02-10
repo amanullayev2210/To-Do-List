0cc134291dfcaf8a0d8701762a2a55d713a6173a
amanullayev2210 <karory577@gmail.com>
2025-02-11 00:47:52 +0500

    Update JS

--- assets/script.js
+++ assets/script.js
@@ -12,18 +12,3 @@ form.addEventListener("submit", (evt) => {
   num++;
 });
 
-// const form = document.querySelector(".js-form")
-// const  input = document.querySelector(".js-input");
-// const wrapper = document.querySelector(".js-ul");
-// const template = document.querySelector(".js-template").content;
-
-// form.addEventListener("submit", (evt) => {
-//   evt.preventDefault();
-//   const docFrg = document.createDocumentFragment();
-//   const clone = template.cloneNode(true);
-//   clone.querySelector(".js-li").textContent = input.value;
-//   docFrg.appendChild(clone);
-//   wrapper.appendChild(docFrg);
-
-//   input.value = null;
-// });
