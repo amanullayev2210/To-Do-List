8c27047ac2ddc6ea2a346702895328c0216b4c29
amanullayev2210 <karory577@gmail.com>
2025-02-14 23:05:55 +0500

    Add contact JS

--- assets/contact.js
+++ assets/contact.js
@@ -1,5 +1,8 @@
-const new_form = document.querySelector(".js-form");
-const new_input = document.querySelector(".js-new_input");
+const new_form = document.querySelector(".new-js_form");
+const new_input = document.querySelector(".new-js_input");
+const new_input2 = document.querySelector(".new-js_input2")
+const new_text = document.querySelector(".new-js_text")
+const new_button = document.querySelector(".new-js_button")
 const new_ul = document.querySelector(".js-new_ul");
 const new_template = document.querySelector(".js-new_template").content;
  
@@ -8,8 +11,12 @@ let arr = [];
 new_form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   arr.push(new_input.value);
+  arr.push(new_input2.value)
+  arr.push(new_text.value)
   renderText();
   new_input.value = "";
+  new_input2.value = "";
+  new_text.value = "";
 });
 
 function renderText() {
@@ -19,7 +26,7 @@ function renderText() {
 
   arr.forEach((item, index) => {
     const clone = new_template.cloneNode(true);
-    clone.querySelector(".js-paragraph").textContent = `${index + 1}. ${item}`;
+    clone.querySelector(".js-paragraph").textContent = `${index + 1}.  ${item}`;
     clone.querySelector(".js-button").addEventListener("click", (evt) => {
       evt.preventDefault();
       arr = arr.filter((_, inx) => index !== inx);
